a039df72b8c49bd6d15bdc137abe683c33e0388b
alaa <aamal79545@gmail.com>
2024-02-09 21:30:03 +0200

    Add project files.

---
+++ InsertAtBeginning/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InsertAtBeginning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode InsertAtBeginning(ListNode head, ListNode newElement)
+        {
+            ListNode newNode = new ListNode();
+            newNode.val = newElement.val;
+            newNode.next = head;
+            head = newNode;
+
+            return head;
+        }
+    }
+}
